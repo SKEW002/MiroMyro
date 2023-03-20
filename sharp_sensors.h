f62c5536cc59fab99ef1106044ab608ab5b86777
Albert <aojd25@gmail.com>
2023-03-20 16:15:49 +0800

    Added analog to cm conversion

--- sharp_sensors.h
+++ sharp_sensors.h
@@ -1,6 +1,7 @@
 #ifndef SHARP_SENSORS_H
 #define SHARP_SENSORS_H
 
+
 int analog_to_cm(int x){
 	int m = 1;
 	int c = 0;
@@ -18,5 +19,30 @@ int distance_front_top(){
 	return SensorValue(sharp_front_top);
 }
 */
+
+
+int sensor_dist(int sensor_no){
+	int dist;
+	int value = SensorValue(sharp_front_bottom);
+
+	if (sensor_no == 1){
+		dist = 160339 * (float) pow(value, -1.285);
+
+	}
+	else if (sensor_no == 2){
+		dist = 180581 * (float) pow(value,-1.299);
+
+	}
+	else if (sensor_no == 3){
+		dist = 101263 * (float) pow(value,-1.226);
+
+	}
+	else if (sensor_no == 4){
+		dist = 27724 * (float) pow(value,-1.153);
+
+	}
+
+	return dist;
+}
 
 #endif
